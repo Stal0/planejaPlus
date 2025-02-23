ad102294a805017a61a1331f3878183352c6905c
Lucas <lucasgabrielsantos398@gmail.com>
2025-02-23 10:53:24 -0300

    find user by id controllet method

---
+++ src/main/java/com/stal0/planejaPlus/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.stal0.planejaPlus.resources;
+
+
+import com.stal0.planejaPlus.dto.UserDTO;
+import com.stal0.planejaPlus.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(name = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+        UserDTO user = userService.getUserById(id);
+        return ResponseEntity.ok().body(user);
+    }
+}
